2d8a43518e0a7f7193ee60ea77387df3bbb08b03
prasanta1987 <prasanta.1987@hotmail.com>
2018-11-18 22:59:04 +0530

    features added

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ const searchBtn = document.querySelector('#searchbtn')
 const cardContainer = document.querySelector('#cardcontainer')
 const apprevoedReports = document.querySelector('#apprevedreports')
 const disApprevoedReports = document.querySelector('#disapprevedreports')
+const totalIncomeDom = document.querySelector('#totalincome')
 
+let totalIncome = 0;
 
 //Set Up Flipkart Affiliate API
 var flipkartClient = new flipkart.CreateAffiliateClient({
@@ -31,40 +33,53 @@ approvedStatus.forEach((Status) => {
             value = JSON.parse(value.body).orderList
             value.forEach((doc) => {
                 OderReports(doc, Status)
+                if(Status == 'approved'){
+                    totalIncome = totalIncome + doc.tentativeCommission.amount;
+                }
             })
+            getTotalIncome(totalIncome)
         })
         .catch(function (err) {
             console.log(err);
         });
 })
 
+function getTotalIncome(amount){
+    totalIncomeDom.innerHTML = `
+    <div class="col-sm-12 bg-primary text-light rounded p-1">
+    <h1 class="text-center">Total Income - ${amount}</h1>
+    </div>
+    `
+}
+
 function OderReports(doc, Selector) {
-    console.log(doc)
     soldItem = doc.title
     soldItemCategory = doc.category
     soldItemPrice = doc.sales.amount
     commission = doc.tentativeCommission.amount
     orderStatus = doc.status
-
+    percentage = ((commission / soldItemPrice) * 100).toFixed(2) + '%'
     const div = document.createElement('div')
     div.className = 'col-sm-12'
     div.id = 'approvedlist'
     div.innerHTML = `
-    <h6>${soldItem}</h6>
+    <p class="w-100 text-center"><b>${soldItem}</b></p>
+    <hr class="bg-light">
     <ul id="approvedItemElement">
-        <li>Category: ${soldItemCategory}</li>
-        <li>Selling Price: ${soldItemPrice}</li>
-        <li>Commission: ${commission}</li>
-        <li>Order Status: ${orderStatus}</li>
+        <li>Category : <b>${soldItemCategory}</b></li>
+        <li>Selling Price : <b>${soldItemPrice}</b></li>
+        <li>Commission : <b>${commission}</b></li>
+        <li>Commission Percentage: <b>${percentage}</b></li>
+        <li>Order Status : <b>${orderStatus}</b></li>
     </ul>
     `
     if (Selector == 'approved') {
         div.className = 'border rounded border-light bg-success text-light'
-        div.style.padding = '10px'
+        div.style.padding = '5px 0px 0px 0px'
         apprevoedReports.appendChild(div)
     } else {
         div.className = 'border rounded border-light bg-danger text-light'
-        div.style.padding = '10px'
+        div.style.padding = '5px 0px 0px 0px'
         disApprevoedReports.appendChild(div)
     }
 }
@@ -72,8 +87,13 @@ function OderReports(doc, Selector) {
 //Search Event Listner
 searchBtn.addEventListener('click', () => {
     const query = itemName.value
+    filpkartSecarc(query)
+})
 
-    flipkartClient.doKeywordSearch(query, 10)
+filpkartSecarc()
+
+function filpkartSecarc(querry = '') {
+    flipkartClient.doKeywordSearch(querry, 10)
         .then(function (value) {
             cardContainer.innerHTML = ''
             flData = JSON.parse(value.body)
@@ -82,7 +102,7 @@ searchBtn.addEventListener('click', () => {
         .catch(function (err) {
             console.log(err);
         });
-})
+}
 
 //Search Event Listner callback function
 function flipkartFetchData(data) {
@@ -101,12 +121,13 @@ function flipkartFetchData(data) {
 
         //Creating New DOM Elements
         let cardbody = document.createElement('div')
-        cardbody.className = 'row border rounded'
+        cardbody.className = 'col-sm-12 border rounded bg-light'
         cardbody.style.padding = '10px'
         cardbody.style.marginBottom = '10px'
         cardbody.innerHTML = `
+            <div class="row">
             <div class="col-sm-2 text-center">
-                <img class="img-fluid" src="${itemImage}" alt="Card image">
+                <img class="img-fluid rounded" src="${itemImage}" alt="Card image">
             </div>
             <div class="col-sm-10">
                 <h5><b>${itemTitle}</b></h5>
@@ -120,11 +141,12 @@ function flipkartFetchData(data) {
                 <hr>
                 <div class="row" id="pricecontainer${i}">
                     <div class="col-sm">
-                        <small>Maximum Retail Price: ${mrp} ${currencyFormat}</small></br>
-                        <small>Selling Price: ${flSellPrice} ${currencyFormat}</small></br>
-                        <small>Flipkart Special Price: ${flSpecialPrice} ${currencyFormat}</small>
+                        <small>Maximum Retail Price : <b>${mrp} ${currencyFormat}</b></small></br>
+                        <small>Selling Price : <b>${flSellPrice} ${currencyFormat}</b></small></br>
+                        <small>Flipkart Special Price : <b>${flSpecialPrice} ${currencyFormat}</b></small>
                     </div>
                 </div>
+            </div>
             </div>
             `
         cardContainer.appendChild(cardbody)
